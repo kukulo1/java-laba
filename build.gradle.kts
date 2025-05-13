878c9a9f6d8a5f597ab1994eebfa4a1f066fcfd0
kukulo1 <kukulo1@Mail.ru>
2025-05-13 16:49:39 +0300

    господи помилуй я заебался

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,3 @@ dependencies {
     // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
     implementation("com.mysql:mysql-connector-j:9.2.0")
 }
-
-tasks.test {
-    useJUnitPlatform()
-}
-application {
-    mainClass = "ru/programming/Main"
-}
